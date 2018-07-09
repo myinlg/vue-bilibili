39727b15a24a6fefec2d454fcf3862dae1a8b9f3
Sailor <28142673@qq.com>
2018-07-09 10:29:58 +0800

    添加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import routers from './router'
 Vue.use(Router)
-
-export default new Router({
+const router = new Router({
     routes: routers
 })
+router.beforeResolve((to, from, next) => {
+    let userName = util.getCookie('username')
+    if (!userName && to.path !== '/login') {
+        next({
+            path: '/login',
+            query: {
+                redirect: to.fullPath
+            }
+        })
+    }
+    next()
+})
+export default router
