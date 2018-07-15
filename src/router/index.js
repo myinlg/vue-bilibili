8259296624b61b54e60c0811935189038153e146
Sailor <28141673@qq.com>
2018-07-15 19:17:57 +0800

    注释路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,18 @@ import Router from 'vue-router'
 import routers from './router'
 Vue.use(Router)
 const router = new Router({
-    routes: routers
+  routes: routers
 })
-router.beforeResolve((to, from, next) => {
-    let userName = util.getCookie('username')
-    if (!userName && to.path !== '/login') {
-        next({
-            path: '/login',
-            query: {
-                redirect: to.fullPath
-            }
-        })
-    }
-    next()
-})
+// router.beforeResolve((to, from, next) => {
+//     let userName = util.getCookie('username')
+//     if (!userName && to.path !== '/login') {
+//         next({
+//             path: '/login',
+//             query: {
+//                 redirect: to.fullPath
+//             }
+//         })
+//     }
+//     next()
+// })
 export default router
